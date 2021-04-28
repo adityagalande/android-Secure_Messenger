8b9500d9f0d25823c370bbf5e1842369d74d4348
Aditya Galande <adityagalande14@gmail.com>
2021-04-28 22:52:22 +0530

    storage dependancy and initialization

--- app/src/main/java/com/android/firebase/securemessenger/MainActivity.java
+++ app/src/main/java/com/android/firebase/securemessenger/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.InputFilter;
@@ -28,6 +29,8 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -40,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
     public static final int DEFAULT_MSG_LENGTH_LIMIT = 1000;
 
     public static final int RC_SIGN_IN = 1;
+    private static final int RC_PHOTO_PICKER =  2;
 
     private ListView mMessageListView;
     private MessageAdapter mMessageAdapter;
@@ -63,6 +67,11 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mFirebaseAuth;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
 
+
+    //to get instance of firebase storage service to open access
+    private FirebaseStorage mFirebaseStorage;
+    private StorageReference mChatPhotosStorageReference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -76,8 +85,13 @@ public class MainActivity extends AppCompatActivity {
         //This is main access point to AUTH(open access)
         mFirebaseAuth = FirebaseAuth.getInstance();
 
+        //This is main access point to storage(open access)
+        mFirebaseStorage = FirebaseStorage.getInstance();
+
         //get referance of access point and get child object
         mMessageDatabaseReference = mfirebaseDatabase.getReference().child("message");
+        //same acts as abouv function
+        mChatPhotosStorageReference = mFirebaseStorage.getReference().child("chat_photos");
 
         // Initialize references to views
         mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
@@ -99,7 +113,11 @@ public class MainActivity extends AppCompatActivity {
         mPhotoPickerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                //To get image from local storage of device
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/jpeg");
+                intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+                startActivityForResult(Intent.createChooser(intent, "Complete action using"), RC_PHOTO_PICKER);
             }
         });
 
@@ -196,6 +214,9 @@ public class MainActivity extends AppCompatActivity {
             } else if (resultCode == RESULT_CANCELED) {
                 Toast.makeText(MainActivity.this, "Signed-Out", Toast.LENGTH_LONG).show();
                 finish();
+            } else if (requestCode == RC_PHOTO_PICKER && requestCode == RESULT_OK) {
+                Uri selectedImageUri = data.getData();
+                StorageReference photoReferance = mChatPhotosStorageReference.child(selectedImageUri.getLastPathSegment());
             }
         }
     }
